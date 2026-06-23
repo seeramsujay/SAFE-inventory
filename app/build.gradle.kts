1920ce4f274cd2f26d393e8f6b090970f23feb69
Lochan B <lochan.bhavanasi@gmail.com>
2026-06-23 22:48:58 +0530

    modified:   app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
   buildFeatures {
     compose = true
@@ -59,7 +59,7 @@ android {
 }
 
 kotlin {
-  jvmToolchain(17)
+  jvmToolchain(21)
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
